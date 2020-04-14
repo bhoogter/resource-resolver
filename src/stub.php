85bdac2d9a4520ac8f983fbf4042d65465f5e936
ben <bhoogter@yahoo.com>
2020-04-13 20:43:33 -0400

    Fix tests

--- src/stub.php
+++ src/stub.php
@@ -6,7 +6,7 @@ dependency_manager("resource_resolver", null, __DIR__ . "/phars/");
 
 spl_autoload_register(function ($name) {
     $d = (strpos(__FILE__, ".phar") === false ? __DIR__ : "phar://" . __FILE__ . "/src");
-    if ($name == "resource_resolver") require_once($d . "/support/class-resource-resolver.php");
+    if ($name == "resource_resolver") require_once($d . "/class-resource-resolver.php");
 });
 
 __HALT_COMPILER();
