3df1e6148aae344ca946a1035b1b6c3b637db0cd
ben <bhoogter@yahoo.com>
2020-04-14 16:27:58 -0400

    Remove unneeded dependencies

--- src/stub.php
+++ src/stub.php
@@ -1,8 +1,4 @@
 <?php
-// Place the dependency manager phar in the same directory ()
-define('DEPENDENCY_MANAGER_PHAR', __DIR__ . "/phars/php-dependency-manager.phar");
-require_once("phar://" . DEPENDENCY_MANAGER_PHAR . "/src/class-dependency-manager.php");
-dependency_manager("resource_resolver", null, __DIR__ . "/phars/");
 
 spl_autoload_register(function ($name) {
     $d = (strpos(__FILE__, ".phar") === false ? __DIR__ : "phar://" . __FILE__ . "/src");
