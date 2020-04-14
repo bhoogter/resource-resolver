5224ab6929681b9c8667ca50a6373fc070b4f382
ben <bhoogter@yahoo.com>
2020-04-13 20:32:32 -0400

    Clean up

--- src/stub.php
+++ src/stub.php
@@ -2,12 +2,10 @@
 // Place the dependency manager phar in the same directory ()
 define('DEPENDENCY_MANAGER_PHAR', __DIR__ . "/phars/php-dependency-manager.phar");
 require_once("phar://" . DEPENDENCY_MANAGER_PHAR . "/src/class-dependency-manager.php");
-dependency_manager("pages", null, __DIR__ . "/phars/");
+dependency_manager("resource_resolver", null, __DIR__ . "/phars/");
 
 spl_autoload_register(function ($name) {
     $d = (strpos(__FILE__, ".phar") === false ? __DIR__ : "phar://" . __FILE__ . "/src");
-    if ($name == "xml_serve") require_once($d . "/class-xml-serve.php");
-    if ($name == "page_render") require_once($d . "/support/class-page-render.php");
     if ($name == "resource_resolver") require_once($d . "/support/class-resource-resolver.php");
 });
 
